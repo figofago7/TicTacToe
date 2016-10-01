7a5d8cefe15e65e7176847bc14e180d73c231c20
figofago7 <mateuszrado7@gmail.com>
2016-10-01 12:26:29 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pole1 = new System.Windows.Forms.PictureBox();
+            this.pole2 = new System.Windows.Forms.PictureBox();
+            this.pole3 = new System.Windows.Forms.PictureBox();
+            this.pole6 = new System.Windows.Forms.PictureBox();
+            this.pole5 = new System.Windows.Forms.PictureBox();
+            this.pole4 = new System.Windows.Forms.PictureBox();
+            this.pole9 = new System.Windows.Forms.PictureBox();
+            this.pole8 = new System.Windows.Forms.PictureBox();
+            this.pole7 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pole10 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pole1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pole1
+            // 
+            this.pole1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole1.Location = new System.Drawing.Point(33, 49);
+            this.pole1.Name = "pole1";
+            this.pole1.Size = new System.Drawing.Size(100, 100);
+            this.pole1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole1.TabIndex = 0;
+            this.pole1.TabStop = false;
+            this.pole1.Click += new System.EventHandler(this.pole1_Click);
+            // 
+            // pole2
+            // 
+            this.pole2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole2.Location = new System.Drawing.Point(139, 49);
+            this.pole2.Name = "pole2";
+            this.pole2.Size = new System.Drawing.Size(100, 100);
+            this.pole2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole2.TabIndex = 1;
+            this.pole2.TabStop = false;
+            this.pole2.Click += new System.EventHandler(this.pole2_Click);
+            // 
+            // pole3
+            // 
+            this.pole3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole3.Location = new System.Drawing.Point(245, 49);
+            this.pole3.Name = "pole3";
+            this.pole3.Size = new System.Drawing.Size(100, 100);
+            this.pole3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole3.TabIndex = 2;
+            this.pole3.TabStop = false;
+            this.pole3.Click += new System.EventHandler(this.pole3_Click);
+            // 
+            // pole6
+            // 
+            this.pole6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole6.Location = new System.Drawing.Point(245, 155);
+            this.pole6.Name = "pole6";
+            this.pole6.Size = new System.Drawing.Size(100, 100);
+            this.pole6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole6.TabIndex = 5;
+            this.pole6.TabStop = false;
+            this.pole6.Click += new System.EventHandler(this.pole6_Click);
+            // 
+            // pole5
+            // 
+            this.pole5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole5.Location = new System.Drawing.Point(139, 155);
+            this.pole5.Name = "pole5";
+            this.pole5.Size = new System.Drawing.Size(100, 100);
+            this.pole5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole5.TabIndex = 4;
+            this.pole5.TabStop = false;
+            this.pole5.Click += new System.EventHandler(this.pole5_Click);
+            // 
+            // pole4
+            // 
+            this.pole4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole4.Location = new System.Drawing.Point(33, 155);
+            this.pole4.Name = "pole4";
+            this.pole4.Size = new System.Drawing.Size(100, 100);
+            this.pole4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole4.TabIndex = 3;
+            this.pole4.TabStop = false;
+            this.pole4.Click += new System.EventHandler(this.pole4_Click);
+            // 
+            // pole9
+            // 
+            this.pole9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole9.Location = new System.Drawing.Point(245, 261);
+            this.pole9.Name = "pole9";
+            this.pole9.Size = new System.Drawing.Size(100, 100);
+            this.pole9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole9.TabIndex = 8;
+            this.pole9.TabStop = false;
+            this.pole9.Click += new System.EventHandler(this.pole9_Click);
+            // 
+            // pole8
+            // 
+            this.pole8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole8.Location = new System.Drawing.Point(139, 261);
+            this.pole8.Name = "pole8";
+            this.pole8.Size = new System.Drawing.Size(100, 100);
+            this.pole8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole8.TabIndex = 7;
+            this.pole8.TabStop = false;
+            this.pole8.Click += new System.EventHandler(this.pole8_Click);
+            // 
+            // pole7
+            // 
+            this.pole7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pole7.Location = new System.Drawing.Point(33, 261);
+            this.pole7.Name = "pole7";
+            this.pole7.Size = new System.Drawing.Size(100, 100);
+            this.pole7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole7.TabIndex = 6;
+            this.pole7.TabStop = false;
+            this.pole7.Click += new System.EventHandler(this.pole7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(406, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ruch:";
+            // 
+            // pole10
+            // 
+            this.pole10.InitialImage = ((System.Drawing.Image)(resources.GetObject("pole10.InitialImage")));
+            this.pole10.Location = new System.Drawing.Point(400, 122);
+            this.pole10.Name = "pole10";
+            this.pole10.Size = new System.Drawing.Size(100, 100);
+            this.pole10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pole10.TabIndex = 10;
+            this.pole10.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(377, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 60);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Wyczyść";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 409);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pole10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pole9);
+            this.Controls.Add(this.pole8);
+            this.Controls.Add(this.pole7);
+            this.Controls.Add(this.pole6);
+            this.Controls.Add(this.pole5);
+            this.Controls.Add(this.pole4);
+            this.Controls.Add(this.pole3);
+            this.Controls.Add(this.pole2);
+            this.Controls.Add(this.pole1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pole1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pole10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pole1;
+        private System.Windows.Forms.PictureBox pole2;
+        private System.Windows.Forms.PictureBox pole3;
+        private System.Windows.Forms.PictureBox pole6;
+        private System.Windows.Forms.PictureBox pole5;
+        private System.Windows.Forms.PictureBox pole4;
+        private System.Windows.Forms.PictureBox pole9;
+        private System.Windows.Forms.PictureBox pole8;
+        private System.Windows.Forms.PictureBox pole7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pole10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        char p1, p2, p3, p4, p5, p6, p7, p8, p9;    //kolejne pola planszy ('n' - nic, 'o' - kolko, 'x' - krzyzyk)
+        char ruch;  //kto wykonuje ruch ('o' - kolko, 'x' - krzyzyk)
+        int ilosc = 9; //ilosc ruchow pozostala do konca gry
+
+        void sprawdz() //funkcja sprawdzajaca, czy nastapila wygrana
+        {
+            if ((p1 == p2 && p2 == p3 && p1 != 'n') ||
+               (p4 == p5 && p5 == p6 && p4 != 'n') ||
+               (p7 == p8 && p8 == p9 && p7 != 'n') ||
+               (p1 == p4 && p4 == p7 && p1 != 'n') ||
+               (p2 == p5 && p5 == p8 && p2 != 'n') ||
+               (p3 == p6 && p6 == p9 && p3 != 'n') ||
+               (p1 == p5 && p5 == p9 && p1 != 'n') ||
+               (p3 == p5 && p5 == p7 && p3 != 'n'))
+            {
+                string wiadomosc;
+                if (ruch == 'o')
+                    wiadomosc = "Wygrywa krzyżyk.";
+                else
+                    wiadomosc = "Wygrywa kółko.";
+
+                if (pole1.Enabled == true)
+                    pole1.Enabled = false;
+                if (pole2.Enabled == true)
+                    pole2.Enabled = false;
+                if (pole3.Enabled == true)
+                    pole3.Enabled = false;
+                if (pole4.Enabled == true)
+                    pole4.Enabled = false;
+                if (pole5.Enabled == true)
+                    pole5.Enabled = false;
+                if (pole6.Enabled == true)
+                    pole6.Enabled = false;
+                if (pole7.Enabled == true)
+                    pole7.Enabled = false;
+                if (pole8.Enabled == true)
+                    pole8.Enabled = false;
+                if (pole9.Enabled == true)
+                    pole9.Enabled = false;
+                MessageBox.Show(wiadomosc, "Koniec Gry.");
+            }
+            else if (ilosc == 0)
+            {
+                pole10.Image = TicTacToe.Properties.Resources.nic;
+                MessageBox.Show("Remis", "Koniec Gry.");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            //inicjalizacja warstwy graficznej
+            pole1.Image = TicTacToe.Properties.Resources.nic;
+            pole2.Image = TicTacToe.Properties.Resources.nic;
+            pole3.Image = TicTacToe.Properties.Resources.nic;
+            pole4.Image = TicTacToe.Properties.Resources.nic;
+            pole5.Image = TicTacToe.Properties.Resources.nic;
+            pole6.Image = TicTacToe.Properties.Resources.nic;
+            pole7.Image = TicTacToe.Properties.Resources.nic;
+            pole8.Image = TicTacToe.Properties.Resources.nic;
+            pole9.Image = TicTacToe.Properties.Resources.nic;
+            pole10.Image = TicTacToe.Properties.Resources.ooo;
+
+            //inicjalizacja logiki gry
+            p1 = 'n'; p2 = 'n'; p3 = 'n';
+            p4 = 'n'; p5 = 'n'; p6 = 'n';
+            p7 = 'n'; p8 = 'n'; p9 = 'n';
+            ruch = 'o';
+
+            //czyszczenie pola gry (dopisane do buttona Wyczysc - button1)
+            pole1.Enabled = true;
+            pole2.Enabled = true;
+            pole3.Enabled = true;
+            pole4.Enabled = true;
+            pole5.Enabled = true;
+            pole6.Enabled = true;
+            pole7.Enabled = true;
+            pole8.Enabled = true;
+            pole9.Enabled = true;
+
+            ilosc = 9;
+        }
+
+        private void pole1_Click(object sender, System.EventArgs e)
+        {
+            if (p1 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p1 = 'o';
+                    pole1.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p1 = 'x';
+                    pole1.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole1.Enabled = false;
+                ilosc--;
+                sprawdz();
+           }
+        }
+
+        private void pole2_Click(object sender, System.EventArgs e)
+        {
+            if (p2 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p2 = 'o';
+                    pole2.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p2 = 'x';
+                    pole2.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole2.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole3_Click(object sender, System.EventArgs e)
+        {
+            if (p3 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p3 = 'o';
+                    pole3.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p3 = 'x';
+                    pole3.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole3.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole4_Click(object sender, System.EventArgs e)
+        {
+            if (p4 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p4 = 'o';
+                    pole4.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p4 = 'x';
+                    pole4.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole4.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole5_Click(object sender, System.EventArgs e)
+        {
+            if (p5 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p5 = 'o';
+                    pole5.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p5 = 'x';
+                    pole5.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole5.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole6_Click(object sender, System.EventArgs e)
+        {
+            if (p6 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p6 = 'o';
+                    pole6.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p6 = 'x';
+                    pole6.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole6.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole7_Click(object sender, System.EventArgs e)
+        {
+            if (p7 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p7 = 'o';
+                    pole7.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p7 = 'x';
+                    pole7.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole7.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole8_Click(object sender, System.EventArgs e)
+        {
+            if (p8 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p8 = 'o';
+                    pole8.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p8 = 'x';
+                    pole8.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole8.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+
+        private void pole9_Click(object sender, System.EventArgs e)
+        {
+            if (p9 == 'n')
+            {
+                if (ruch == 'o')
+                {
+                    p9 = 'o';
+                    pole9.Image = TicTacToe.Properties.Resources.ooo;
+                    pole10.Image = TicTacToe.Properties.Resources.xxx;
+                    ruch = 'x';
+                }
+                else
+                {
+                    p9 = 'x';
+                    pole9.Image = TicTacToe.Properties.Resources.xxx;
+                    pole10.Image = TicTacToe.Properties.Resources.ooo;
+                    ruch = 'o';
+                }
+                pole9.Enabled = false;
+                ilosc--;
+                sprawdz();
+            }
+        }
+    }
+
+        
+    }
+
